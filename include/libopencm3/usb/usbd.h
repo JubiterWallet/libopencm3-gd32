849802ebe5f365672b3cdad8e639c7f069bd2995
YXP-KeyCos <xingping@ftsafe.com>
2022-12-06 11:25:09 +0800

    adjust driver for gd32f470

--- include/libopencm3/usb/usbd.h
+++ include/libopencm3/usb/usbd.h
@@ -42,11 +42,10 @@ LGPL License Terms @ref lgpl_license
 
 BEGIN_DECLS
 
-
 enum usbd_request_return_codes {
-	USBD_REQ_NOTSUPP	= 0,
-	USBD_REQ_HANDLED	= 1,
-	USBD_REQ_NEXT_CALLBACK	= 2,
+  USBD_REQ_NOTSUPP = 0,
+  USBD_REQ_HANDLED = 1,
+  USBD_REQ_NEXT_CALLBACK = 2,
 };
 
 typedef struct _usbd_driver usbd_driver;
@@ -55,8 +54,9 @@ typedef struct _usbd_device usbd_device;
 extern const usbd_driver st_usbfs_v1_usb_driver;
 extern const usbd_driver stm32f107_usb_driver;
 extern const usbd_driver stm32f207_usb_driver;
+extern const usbd_driver gd32f470_usb_driver;
 extern const usbd_driver st_usbfs_v2_usb_driver;
-#define otgfs_usb_driver stm32f107_usb_driver
+#define otgfs_usb_driver gd32f470_usb_driver
 #define otghs_usb_driver stm32f207_usb_driver
 extern const usbd_driver efm32lg_usb_driver;
 extern const usbd_driver efm32hg_usb_driver;
@@ -94,39 +94,38 @@ extern const usbd_driver lm4f_usb_driver;
  * (note the double @e const.)  The first @e const refers to the strings
  * while the second @e const refers to the array.
  */
-extern usbd_device * usbd_init(const usbd_driver *driver,
-			       const struct usb_device_descriptor *dev,
-			       const struct usb_config_descriptor *conf,
-			       const char * const *strings, int num_strings,
-			       uint8_t *control_buffer,
-			       uint16_t control_buffer_size);
+extern usbd_device *usbd_init(const usbd_driver *driver,
+                              const struct usb_device_descriptor *dev,
+                              const struct usb_config_descriptor *conf,
+                              const char *const *strings, int num_strings,
+                              uint8_t *control_buffer,
+                              uint16_t control_buffer_size);
 
 /** Registers a reset callback */
 extern void usbd_register_reset_callback(usbd_device *usbd_dev,
-					 void (*callback)(void));
+                                         void (*callback)(void));
 /** Registers a suspend callback */
 extern void usbd_register_suspend_callback(usbd_device *usbd_dev,
-					   void (*callback)(void));
+                                           void (*callback)(void));
 /** Registers a resume callback */
 extern void usbd_register_resume_callback(usbd_device *usbd_dev,
-					  void (*callback)(void));
+                                          void (*callback)(void));
 /** Registers a SOF callback */
 extern void usbd_register_sof_callback(usbd_device *usbd_dev,
-				       void (*callback)(void));
+                                       void (*callback)(void));
 
 typedef void (*usbd_control_complete_callback)(usbd_device *usbd_dev,
-		struct usb_setup_data *req);
+                                               struct usb_setup_data *req);
 
 typedef enum usbd_request_return_codes (*usbd_control_callback)(
-		usbd_device *usbd_dev,
-		struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
-		usbd_control_complete_callback *complete);
+    usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
+    uint16_t *len, usbd_control_complete_callback *complete);
 
 typedef void (*usbd_set_config_callback)(usbd_device *usbd_dev,
-					 uint16_t wValue);
+                                         uint16_t wValue);
 
 typedef void (*usbd_set_altsetting_callback)(usbd_device *usbd_dev,
-					     uint16_t wIndex, uint16_t wValue);
+                                             uint16_t wIndex, uint16_t wValue);
 
 typedef void (*usbd_endpoint_callback)(usbd_device *usbd_dev, uint8_t ep);
 
@@ -146,8 +145,8 @@ typedef void (*usbd_endpoint_callback)(usbd_device *usbd_dev, uint8_t ep);
  * @return 0 if successful
  */
 extern int usbd_register_control_callback(usbd_device *usbd_dev, uint8_t type,
-					  uint8_t type_mask,
-					  usbd_control_callback callback);
+                                          uint8_t type_mask,
+                                          usbd_control_callback callback);
 
 /* <usb_standard.c> */
 /** Registers a "Set Config" callback
@@ -157,16 +156,17 @@ extern int usbd_register_control_callback(usbd_device *usbd_dev, uint8_t type,
  * @return -1 if no more space was available for callbacks.
  */
 extern int usbd_register_set_config_callback(usbd_device *usbd_dev,
-					  usbd_set_config_callback callback);
+                                             usbd_set_config_callback callback);
 /** Registers a "Set Interface" (alternate setting) callback
  * @param usbd_dev the usb device handle returned from @ref usbd_init
  * @param callback your desired callback function
  */
-extern void usbd_register_set_altsetting_callback(usbd_device *usbd_dev,
-					usbd_set_altsetting_callback callback);
+extern void usbd_register_set_altsetting_callback(
+    usbd_device *usbd_dev, usbd_set_altsetting_callback callback);
 
 /** Registers a non-contiguous string descriptor */
-extern void usbd_register_extra_string(usbd_device *usbd_dev, int index, const char* string);
+extern void usbd_register_extra_string(usbd_device *usbd_dev, int index,
+                                       const char *string);
 
 /* Functions to be provided by the hardware abstraction layer */
 extern void usbd_poll(usbd_device *usbd_dev);
@@ -192,7 +192,7 @@ extern void usbd_disconnect(usbd_device *usbd_dev, bool disconnected);
  * Not all backends support arbitrary addressing anyway.
  */
 extern void usbd_ep_setup(usbd_device *usbd_dev, uint8_t addr, uint8_t type,
-		uint16_t max_size, usbd_endpoint_callback callback);
+                          uint16_t max_size, usbd_endpoint_callback callback);
 
 /** Write a packet
  * @param usbd_dev the usb device handle returned from @ref usbd_init
@@ -202,7 +202,7 @@ extern void usbd_ep_setup(usbd_device *usbd_dev, uint8_t addr, uint8_t type,
  * @return 0 if failed, len if successful
  */
 extern uint16_t usbd_ep_write_packet(usbd_device *usbd_dev, uint8_t addr,
-				const void *buf, uint16_t len);
+                                     const void *buf, uint16_t len);
 
 /** Read a packet
  * @param usbd_dev the usb device handle returned from @ref usbd_init
@@ -212,14 +212,14 @@ extern uint16_t usbd_ep_write_packet(usbd_device *usbd_dev, uint8_t addr,
  * @return Actual # of bytes read
  */
 extern uint16_t usbd_ep_read_packet(usbd_device *usbd_dev, uint8_t addr,
-			       void *buf, uint16_t len);
+                                    void *buf, uint16_t len);
 /** Set/clear STALL condition on an endpoint
  * @param usbd_dev the usb device handle returned from @ref usbd_init
  * @param addr Full EP address (with direction bit)
  * @param stall if 0, clear STALL, else set stall.
  */
 extern void usbd_ep_stall_set(usbd_device *usbd_dev, uint8_t addr,
-			      uint8_t stall);
+                              uint8_t stall);
 
 /** Get STALL status of an endpoint
  * @param usbd_dev the usb device handle returned from @ref usbd_init
@@ -240,4 +240,3 @@ END_DECLS
 #endif
 
 /**@}*/
-
